d996de8f908065fa2523e272187efd857cfbc348
Uzair Ashraf <uzinatorcl@live.com>
2020-07-13 14:04:59 -0700

    added final test

--- test/test.js
+++ test/test.js
@@ -91,4 +91,61 @@ describe('Reactions API', () => {
         })
     })
   })
+  describe('GET /api/reactions/random', () => {
+    it('It should ask for a valid category when category sent', async () => {
+      try {
+        const response = await chai.request(server)
+          .get('/api/reactions/random?category=blahblah')
+        expect(response).to.have.status(400)
+        expect(response.body).to.deep.equal({
+          message: 'Please send a valid category.'
+        })
+      } catch(err) {
+        console.error(err)
+        expect.fail("Error" + JSON.stringify(err))
+      }
+    })
+    it('It should ask for a category when category is sent without value', async () => {
+      try {
+        const response = await chai.request(server)
+          .get('/api/reactions/random?category=')
+        expect(response).to.have.status(400)
+        expect(response.body).to.deep.equal({
+          message: 'Please supply a category.'
+        })
+      } catch (err) {
+        console.error(err)
+        expect.fail("Error" + JSON.stringify(err))
+      }
+    })
+    it('It should respond with a random reaction in the category', async () => {
+      try {
+        const response = await chai.request(server)
+          .get('/api/categories')
+        const category = response.body[Math.floor(Math.random() * response.body.length)]
+        const randomReactionRes = await chai.request(server)
+          .get(`/api/reactions/random?category=${category}`)
+        expect(randomReactionRes).to.have.status(200)
+        expect(randomReactionRes.body).to.be.a('object')
+        expect(randomReactionRes.body).to.have.property('reaction')
+        expect(isValidUrl(randomReactionRes.body.reaction)).to.be.true
+      } catch (err) {
+        console.error(err)
+        expect.fail("Error" + JSON.stringify(err))
+      }
+    })
+    it('It should respond with a random reaction', async () => {
+      try {
+        const response = await chai.request(server)
+                                   .get(`/api/reactions/random`)
+        expect(response).to.have.status(200)
+        expect(response.body).to.be.a('object')
+        expect(response.body).to.have.property('reaction')
+        expect(isValidUrl(response.body.reaction)).to.be.true
+      } catch (err) {
+        console.error(err)
+        expect.fail("Error" + JSON.stringify(err))
+      }
+    })
+  })
 })
